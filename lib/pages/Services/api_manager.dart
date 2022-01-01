716442ca87c496f85bb1212dfd98d1ad0e3ba932
Piyush wani <piyushwani04@gmail.com>
2022-01-01 19:55:33 +0530

    done api manager code getMethod

--- lib/pages/Services/api_manager.dart
+++ lib/pages/Services/api_manager.dart
@@ -1,12 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:news_api_practice/pages/Constant/constant.dart';
+import 'package:news_api_practice/pages/Models/NewsModel.dart';
 
 class APIManager {
-  void getnews() async {
-    var client = http.Client();
-    var responce = await client.get(Constant.url);
-    if (responce.statusCode == 200) {
-      var jsonString = responce.body;
+  Future<NewsModel> getnews() async {
+    var newsModel;
+    try {
+      var client = http.Client();
+      var responce = await client.get(Constant.url);
+      if (responce.statusCode == 200) {
+        var jsonString = responce.body;
+        var jsonMap = jsonDecode(jsonString);
+        newsModel = NewsModel.fromJson(jsonMap);
+      }
+    } catch (e) {
+      return newsModel;
     }
+    return newsModel;
   }
 }
