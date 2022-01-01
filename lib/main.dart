73ee7fbc03a4c535224ea555ce008ad76c7bc9c5
Piyush wani <piyushwani04@gmail.com>
2022-01-01 19:09:28 +0530

    create home page for display news information

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_api_practice/pages/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,11 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        child: Center(
-          child: Text("demo"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
